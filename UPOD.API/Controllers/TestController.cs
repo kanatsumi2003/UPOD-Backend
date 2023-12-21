8faf32e3ff5c72c4b3f71c58b653472d684673ab
kanatsumi2003 <naduccva@gmail.com>
2023-12-22 01:28:40 +0700

    Create test controller to test CI/CD

---
+++ UPOD.API/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UPOD.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        // GET: api/<TestController>
+        [HttpGet]
+        public String PrintText() => "Hello world";
+    }
+}
